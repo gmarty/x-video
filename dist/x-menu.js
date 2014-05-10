8fb8eb732bc6a761865d658e42a00770a4c5c5d9
Guillaume Marty <edo999@gmail.com>
2014-05-10 16:14:13 +0100

    Don't create a UI if the <x-menu> element is not empty

--- dist/x-menu.js
+++ dist/x-menu.js
@@ -1,38 +1,47 @@
 ///<reference path='declarations/xtag.d.ts'/>
+/** @const */ var MENU_MODES;
+(function (MENU_MODES) {
+    MENU_MODES[MENU_MODES["GLOBAL"] = 0] = "GLOBAL";
+    MENU_MODES[MENU_MODES["LOCAL"] = 1] = "LOCAL";
+})(MENU_MODES || (MENU_MODES = {}));
+
 (function () {
     'use strict';
-
-    /** @const */ var MENU_MODES = {
-        GLOBAL: 0,
-        LOCAL: 1
-    };
 
     function init(xMenu) {
         if (xMenu.xtag.mode === null) {
             return;
         }
 
-        if (xMenu.xtag.mode === MENU_MODES.GLOBAL) {
-            xMenu.xtag.parent.playlist.forEach(function (video, index) {
-                var btn = document.createElement('input');
-                btn.type = 'button';
-                btn.dataset.id = index;
-                btn.className = 'btn';
-                btn.value = video.label ? video.label : 'Video ' + (index + 1);
+        if (!xMenu.hasChildNodes()) {
+            // If there is nothing inside the <x-menu>tag, we create a simple navigation menu.
+            if (xMenu.xtag.mode === 0 /* GLOBAL */) {
+                xMenu.xtag.parent.playlist.forEach(function (video, index) {
+                    var btn = document.createElement('input');
+                    btn.type = 'button';
+                    btn.dataset.id = index;
+                    btn.className = 'btn';
+                    btn.value = video.label ? video.label : 'Video ' + (index + 1);
 
-                xMenu.appendChild(btn);
-            });
-        } else if (xMenu.xtag.mode === MENU_MODES.LOCAL) {
-            xMenu.xtag.videoSrcElement.chapterCues.forEach(function (chapter, index) {
-                var btn = document.createElement('input');
-                btn.type = 'button';
-                btn.dataset.id = index;
-                btn.className = 'btn';
-                btn.value = chapter.text ? chapter.text : 'Chapter ' + (index + 1);
+                    xMenu.appendChild(btn);
+                });
+            } else if (xMenu.xtag.mode === 1 /* LOCAL */) {
+                xMenu.xtag.videoSrcElement.chapterCues.forEach(function (chapter, index) {
+                    var btn = document.createElement('input');
+                    btn.type = 'button';
+                    btn.dataset.id = index;
+                    btn.className = 'btn';
+                    btn.value = chapter.text ? chapter.text : 'Chapter ' + (index + 1);
 
-                xMenu.appendChild(btn);
-            });
+                    xMenu.appendChild(btn);
+                });
+            }
         }
+
+        xtag.addEvent(document, 'keydown:keypass(13,27)', function (event) {
+            console.log('Esc', xMenu);
+            xMenu.hide();
+        });
 
         xMenu.xtag.initialized = true;
     }
@@ -49,9 +58,9 @@
                 // @todo Emit an `init` event in x-video and listen it here.
                 setTimeout(function () {
                     if (!xMenu.hasAttribute('for')) {
-                        xMenu.xtag.mode = MENU_MODES.GLOBAL;
+                        xMenu.xtag.mode = 0 /* GLOBAL */;
                     } else {
-                        xMenu.xtag.mode = MENU_MODES.LOCAL;
+                        xMenu.xtag.mode = 1 /* LOCAL */;
 
                         // Get a reference to parent playlist or chapterCues.
                         var targetId = xMenu.getAttribute('for');
@@ -85,15 +94,15 @@
                     return;
                 }
 
-                if (xMenu.xtag.mode === MENU_MODES.GLOBAL) {
-                    var videoIndex = parseInt(menuBtn.dataset.id, 10);
-                    xMenu.xtag.parent.playByIndex(videoIndex);
-                } else if (xMenu.xtag.mode === MENU_MODES.LOCAL) {
-                    var chapterIndex = parseInt(menuBtn.dataset.id, 10);
-                    xMenu.xtag.parent.playChapter(chapterIndex);
+                var index = parseInt(menuBtn.dataset.id, 10);
+
+                if (xMenu.xtag.mode === 0 /* GLOBAL */) {
+                    xMenu.xtag.parent.playByIndex(index);
+                } else if (xMenu.xtag.mode === 1 /* LOCAL */) {
+                    xMenu.xtag.parent.playChapter(index);
                 }
 
-                xMenu.style.display = 'none';
+                xMenu.hide();
             }
         },
         accessors: {},
@@ -109,6 +118,10 @@
                 }
 
                 xMenu.style.display = 'flex';
+            },
+            hide: function () {
+                var xMenu = this;
+                xMenu.style.display = 'none';
             }
         }
     });
--- src/x-menu.ts
+++ src/x-menu.ts
@@ -1,40 +1,43 @@
 ///<reference path='declarations/xtag.d.ts'/>
 
+/** @const */ enum MENU_MODE {
+  GLOBAL,
+  LOCAL
+}
+
 (function() {
   'use strict';
-
-  /** @const */ var MENU_MODES = {
-    GLOBAL: 0, // List the video in the playlist.
-    LOCAL: 1 // Show only the chapter of one video.
-  };
 
   function init(xMenu) {
     if (xMenu.xtag.mode === null) {
       return;
     }
 
-    if (xMenu.xtag.mode === MENU_MODES.GLOBAL) {
-      xMenu.xtag.parent.playlist.forEach(function(video, index) {
-        var btn = document.createElement('input');
-        btn.type = 'button';
-        btn.dataset.id = index;
-        btn.className = 'btn';
-        btn.value = video.label ?
-          video.label : 'Video ' + (index + 1);
+    if (!xMenu.hasChildNodes()) {
+      // If there is nothing inside the <x-menu>tag, we create a simple navigation menu.
+      if (xMenu.xtag.mode === MENU_MODE.GLOBAL) {
+        xMenu.xtag.parent.playlist.forEach(function(video, index) {
+          var btn = document.createElement('input');
+          btn.type = 'button';
+          btn.dataset.id = index;
+          btn.className = 'btn';
+          btn.value = video.label ?
+            video.label : 'Video ' + (index + 1);
 
-        xMenu.appendChild(btn);
-      });
-    } else if (xMenu.xtag.mode === MENU_MODES.LOCAL) {
-      xMenu.xtag.videoSrcElement.chapterCues.forEach(function(chapter, index) {
-        var btn = document.createElement('input');
-        btn.type = 'button';
-        btn.dataset.id = index;
-        btn.className = 'btn';
-        btn.value = chapter.text ?
-          chapter.text : 'Chapter ' + (index + 1);
+          xMenu.appendChild(btn);
+        });
+      } else if (xMenu.xtag.mode === MENU_MODE.LOCAL) {
+        xMenu.xtag.videoSrcElement.chapterCues.forEach(function(chapter, index) {
+          var btn = document.createElement('input');
+          btn.type = 'button';
+          btn.dataset.id = index;
+          btn.className = 'btn';
+          btn.value = chapter.text ?
+            chapter.text : 'Chapter ' + (index + 1);
 
-        xMenu.appendChild(btn);
-      });
+          xMenu.appendChild(btn);
+        });
+      }
     }
 
     xMenu.xtag.initialized = true;
@@ -52,9 +55,9 @@
         // @todo Emit an `init` event in x-video and listen it here.
         setTimeout(function() {
           if (!xMenu.hasAttribute('for')) {
-            xMenu.xtag.mode = MENU_MODES.GLOBAL;
+            xMenu.xtag.mode = MENU_MODE.GLOBAL;
           } else {
-            xMenu.xtag.mode = MENU_MODES.LOCAL;
+            xMenu.xtag.mode = MENU_MODE.LOCAL;
 
             // Get a reference to parent playlist or chapterCues.
             var targetId = xMenu.getAttribute('for');
@@ -88,15 +91,15 @@
           return;
         }
 
-        if (xMenu.xtag.mode === MENU_MODES.GLOBAL) {
-          var videoIndex = parseInt(menuBtn.dataset.id, 10);
-          xMenu.xtag.parent.playByIndex(videoIndex);
-        } else if (xMenu.xtag.mode === MENU_MODES.LOCAL) {
-          var chapterIndex = parseInt(menuBtn.dataset.id, 10);
-          xMenu.xtag.parent.playChapter(chapterIndex);
+        var index = parseInt(menuBtn.dataset.id, 10);
+
+        if (xMenu.xtag.mode === MENU_MODE.GLOBAL) {
+          xMenu.xtag.parent.playByIndex(index);
+        } else if (xMenu.xtag.mode === MENU_MODE.LOCAL) {
+          xMenu.xtag.parent.playChapter(index);
         }
 
-        xMenu.style.display = 'none';
+        xMenu.hide();
       }
     },
     accessors: {
@@ -113,6 +116,10 @@
         }
 
         xMenu.style.display = 'flex';
+      },
+      hide: function() {
+        var xMenu = this;
+        xMenu.style.display = 'none';
       }
     }
   });
